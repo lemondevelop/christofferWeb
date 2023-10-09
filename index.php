32b8554f519a4b25f1c944f883b7cf3825065062
lemondevelop <jonathan@jjosefsson.se>
2023-10-09 19:48:04 +0200

    added start css and menu

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php echo '
+
+<html>
+<head>
+    <title>Draft</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>    
+';
+echo '
+
+<div id="logo"><img src="images/logo.png"></div>
+
+';
+include 'menu.php';
+
+echo '<body>
+<div class="text"><p>';
+include 'textHome.php'; 
+
+echo '</p></div></body>';
+
+
+echo '
+
+<footer><p>COPYRIGHT (©)</p></footer>
+
+';
+
+?>
---
+++ textHome.php
@@ -0,0 +1,5 @@
+<?php
+echo '
+A sole proprietorship, also known as a sole trader, is owned by one person and operates for their benefit. The owner operates the business alone and may hire employees. A sole proprietor has unlimited liability for all obligations incurred by the business, whether from operating costs or judgments against the business. All assets of the business belong to a sole proprietor, including, for example, a computer infrastructure, any inventory, manufacturing equipment, or retail fixtures, as well as any real property owned by the sole proprietor.
+';
+?>
